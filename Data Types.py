711aecc8a36fa1b2224f205e783f26ebdc05e080
shivamm070402 <108880698+shivamm070402@users.noreply.github.com>
2023-11-10 16:32:47 -0300

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+#1 int
+age=32
+#2 float
+weight=72.22
+#3 string
+name='Shivam'
+#4 boolean
+is_boy=True
+#5 list
+list1=[100,200,300]
+#6 dictionary
+dict1={'name':'Shivam','age':32,'city':'Mumbai'}
+print(name)
+print(age)
+print(weight)
+print(is_boy)
+print(list1)
+print(dict1)
+type(name)
+
+
+# In[13]:
+
+
+type(age)
+
+
+# In[12]:
+
+
+type(weight)
+
+
+# In[14]:
+
+
+type(is_boy)
+
+
+# In[15]:
+
+
+type(list1)
+
+
+# In[16]:
+
+
+type(dict1)
+
+
+# In[17]:
+
+
+print(dict1['city'])
+
+
+# In[26]:
+
+
+#7 tuple
+tup=(10,20,30)
+#8 set
+set1={1,2,3,4,5}
+#None
+house_no=None
+#10 complex
+com=1+2j
+#11 range
+num=range(1,6)
+#12 bytes & bytearray
+byte=bytes([65,66,67])
+print(num)
+
+
+# In[20]:
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
