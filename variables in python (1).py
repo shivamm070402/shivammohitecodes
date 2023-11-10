b64d9a6ffe01bfe7e9b9340d3db790fa71a173d7
shivamm070402 <108880698+shivamm070402@users.noreply.github.com>
2023-11-10 08:33:17 -0300

    Add files via upload

---
+++ variables in python (1).py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+#A variable name must start with a letter or the underscore character
+#A variable name cannot start with a number
+#A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+#Variable names are case-sensitive (age, Age and AGE are three different variables)
+
+var1=22
+print(var1)
+
+
+# In[10]:
+
+
+var_1=33
+print(var_1)
+
+
+# In[3]:
+
+
+1var=22
+print(1var)
+
+
+# In[4]:
+
+
+var 1=22
+print(var 1)
+
+
+# In[5]:
+
+
+var_var1=22
+print(var_var1)
+
+
+# In[7]:
+
+
+def=22
+print(def) #def is keyword not syntax
+
+
+# In[12]:
+
+
+deff=22
+print(deff)
+
+
+# In[13]:
+
+
+#In Python, variable names, function names, and keywords are all case-sensitive.
+#This means that if you define a variable "x" and then try to refer to it later as "X", Python will treat it as a different variable, and you'll get an error. 
+#Similarly, if you try to call a function "print" instead of "Print", Python will also give you an error.
+
+x = 5  
+X = 10  
+print(x)  # Output: 5  
+print(X)  # Output: 10  
+
+
+# In[ ]:
+
+
+
+
